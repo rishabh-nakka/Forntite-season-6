1be7d0d2e7f53ceb611f3f51e043978df2014428
rishabh-nakka <76552521+rishabh-nakka@users.noreply.github.com>
2021-04-16 17:15:56 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function jump(){
+    window.location.href="https://youtu.be/nIP2zl1qZhc";
+}
+function sub(){
+    window.location.href="https://www.youtube.com/channel/UCl9hdU60M6Wv3lJm9e8wKyA";
+}
+function yes(){
+    window.location.href="https://www.idp.com/medias/Thank-you-01.jpg?context=bWFzdGVyfGltYWdlc3wzMTkzMjZ8aW1hZ2UvanBlZ3xpbWFnZXMvaDNlL2hjNS85OTU1Nzk4MDI0MjIyLmpwZ3w0ZDQ5OTU0MjkyMjdkYTI2YTVkMmY1NmJiMzUyOWE3OTYzYTU0OTExYzM5NWU2MzE5ZWU0ZTc4ZTJkOTQwNTY0";
+}
+function no(){
+    window.location.href="https://img.devrant.com/devrant/rant/r_1373430_fS7Gi.jpg";
+}
